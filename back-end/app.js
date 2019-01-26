dc7a3d8cbfc66018c01976cfcce95f09d588e81e
Earthii <xiao_eric@hotmail.com>
2019-01-26 16:53:30 -0500

    watson route

--- back-end/app.js
+++ back-end/app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var api = require('./routes/api');
 
 var app = express();
-
+require('dotenv').config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2018-11-16',
+    iam_apikey: process.env.WATSON_API_KEY,
+    url:
+      'https://gateway-wdc.watsonplatform.net/natural-language-understanding/api'
+  });
+
+  var parameters = {
+    text: 'Hey Anna where does this apple go',
+    features: {
+      keywords: {
+        sentiment: true,
+        emotion: true,
+        limit: 3
+      }
+    }
+  };
+
+  naturalLanguageUnderstanding.analyze(parameters, function(err, response) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(response);
+    }
+    // console.log(JSON.stringify(response, null, 2));
+  });
 });
 
 module.exports = router;
